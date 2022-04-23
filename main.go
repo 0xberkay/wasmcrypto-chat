3028effe0d9b81da3ba7e8c789f84c363de0ab1b
0xberkay <70816926+0xberkay@users.noreply.github.com>
2022-04-23 17:04:10 +0300

    files uploaded

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
-	"embed"
 	"log"
-	"net/http"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/filesystem"
 	"github.com/gofiber/websocket/v2"
 )
-
-//go:embed web/*
-var embedDirStatic embed.FS
 
 func main() {
 
@@ -19,10 +13,8 @@ func main() {
 	go websocketHub()
 	app.Use("/ws", WebsocketUpgrade)
 	app.Get("/ws/chat", websocket.New(WebSocketRun))
-	app.Use("/static", filesystem.New(filesystem.Config{
-		Root:   http.FS(embedDirStatic),
-		Browse: true,
-	}))
+
+	app.Static("/", "./web")
 
 	// ws://localhost:3000/ws
 	log.Fatal(app.Listen(":3000"))
